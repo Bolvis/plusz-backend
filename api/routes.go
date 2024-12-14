c959803431fe3966ed5495ee4913f92a2b4c9f1f
Dawid Stasiak <dawid.stasiak@sollers.eu>
2024-12-14 13:17:03 +0100

    change server port from the default HTTP port

--- api/routes.go
+++ api/routes.go
@@ -10,7 +10,7 @@ func Init() {
 
 	router.POST("/addScheduleVersion", service.GetSchedule)
 
-	if err := router.Run(":8080"); err != nil {
+	if err := router.Run(":2013"); err != nil {
 		panic(err)
 	}
 }
